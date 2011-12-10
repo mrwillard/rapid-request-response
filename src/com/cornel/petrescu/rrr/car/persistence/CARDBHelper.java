34f7ed42718328afb839f744598141dd6a4857d0
cornel.petrescu.mind <cornel.petrescu.mind@gmail.com>
2011-12-10 08:55:19 +0000

    Removed warnings, some leftover comments.

--- src/com/cornel/petrescu/rrr/car/persistence/CARDBHelper.java
+++ src/com/cornel/petrescu/rrr/car/persistence/CARDBHelper.java
@@ -1,6 +1,5 @@
 package com.cornel.petrescu.rrr.car.persistence;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
